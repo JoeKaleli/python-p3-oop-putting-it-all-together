4c182f83d64c0f1b1657afed777d73460a9e7e75
JoeKaleli <kalelimburu9@gmail.com>
2024-03-18 12:48:28 +0300

    Done only one failing

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+
+    def __init__(self,brand,size):
+        self.brand = brand
+        self.size = size
+            
+    
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+    def get_size(self):
+        try :
+            return self._size
+        except:
+            return
+
+    def set_size(self,size):
+        if type(size) == int:
+            self._size = size
+        else: 
+            print("size must be an integer")
+
+    size = property(get_size, set_size)
